607aaf2f9f2c84ea755db53c69f2f29f70142459
abdulbasit221 <abdulbasitalikhan598@gmail.com>
2023-10-03 18:53:18 +0500

    news data

---
+++ admin/delete-user.php
@@ -0,0 +1,13 @@
+<?php 
+
+include "config.php";
+$id= $_GET['id'];
+
+$Dlt_query = "DELETE FROM `user` WHERE user_id = $id";
+$Dlt_result = mysqli_query($conn , $Dlt_query);
+
+if($Dlt_result){
+    header("location: users.php");
+}
+
+?>
